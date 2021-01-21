5690c838cf4322782df200a5fa24de0aee8bc65b
Website1908 <77770776+Website1908@users.noreply.github.com>
2021-01-21 13:03:45 +0530

    Add files via upload

---
+++ transactions.php
@@ -0,0 +1,189 @@
+
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>MONEY PLUS</title> 
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+<meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type"> 
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+
+<script type="text/javascript" src="http://me.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=XXd-Py6ExnjFwS77Pe4mURWR2_rok1xS9zVDylHxylZJIwxpXQzbqLxSVB99X6DfBHV_qnXkbIEgNCTaMLbiVg" charset="UTF-8"></script><style> 
+
+.mySlides {display:none;} 
+
+body {font-family:Times New Roman;} 
+
+.navbar { 
+
+width:100%; 
+
+background-color: #f2f2f2; 
+
+overflow: auto; 
+
+} 
+.navbar a{ float:right; 
+padding:16px; 
+color:#000066; 
+text-decoration:none; 
+font-size:20px; 
+} 
+.navbar a:hover { 
+backgound-color:#0000cc; 
+} 
+@media screen and (max-width: 500px) 
+.navbar a { 
+float:none; 
+enter display:block; 
+} 
+} 
+</style> 
+</head> 
+<style> 
+body { 
+background-image:url('Bg.jpg'); 
+background-repeat:no-repeat; 
+background-attachment:fixed; 
+background-size:100% 100%; 
+} 
+</style> 
+<body> 
+<h4 style="color:#660066; text-align:left; font-family:Times New Roman;">Money Plus<i class="fa fa-university"></i></h4> 
+<div class="navbar"> 
+<a href ="transaction_history.php">Transaction History</a> 
+<a href ="transfer-money.php">Transfer Money</a> 
+<a href ="create-user.php">Create user</a> 
+<a href= "home.php">Home</a>  
+</div> 
+<style> 
+table { 
+font-family: Tmes New Roman, sans-serif; 
+border-collapse: collapse; 
+width: 75%; 
+} 
+
+td, th { 
+border: 1px solid #000080; 
+text-align: center; 
+padding: 4px; 
+} 
+
+tr:nth-child(even) { 
+background-color: #f2f2f2; 
+} 
+
+</style> 
+</head> 
+<body> 
+<div class="container">
+<h2 style ="color:#990000;text-align:left;font-family: Times New Roman;"><b>Transfer Money</b></h2>
+        <br>
+            <div class="row">
+                <div class="col">
+                    <div class="table-responsive-sm">
+                    <table class="table table-hover table-sm table-striped table-condensed table-bordered">
+                        <thead>
+                            <tr>
+                            <th scope="col" class="text-center py-2"><h5><b> ID</h5></b></th>
+                            <th scope="col" class="text-center py-2"><h5 ><b>Name</h5></b></th>
+                            <th scope="col" class="text-center py-2"><h5 ><b>E-mail</h5></b></th>
+                            <th scope="col" class="text-center py-2"><h5 ><b>Balance</h5></b></th>
+       
+                            </tr>
+
+<tr>
+                        <td class="py-2">1</td>
+                        <td class="py-2">Shubhra</td>
+                        <td class="py-2">kulkarni@gmail.com</td>
+                        <td class="py-2">40000</td>
+                        </tr>
+</body>
+</table>
+</section>
+<br>
+<style> 
+body {font-family: TimesNewRoman;} 
+* {box-sizing: border-box;} 
+/* Add styles to the form container */ 
+.container { 
+position: left; 
+right: 0; 
+margin: 20px; 
+max-width: 950px; 
+padding: 16px; 
+background-color: AntiqueWhite; 
+} 
+
+input[type=text], select, textarea { 
+width: 100%; 
+padding: 15px; 
+border: 1px solid #3333cc; 
+border-radius: 2px; 
+box-sizing: border-box; 
+margin-top: 4px; 
+margin-bottom: 10px; 
+resize: horizontal; 
+background-color: white; 
+} 
+.container1 { 
+position: center; 
+right: 0; 
+text-align: center; 
+} 
+input[type=submit] { 
+background-color: White; 
+color: Marroon; 
+padding: 12px 20px; 
+border-color: #000033; 
+border-radius: 4px; 
+cursor: pointer; 
+position:center; 
+} 
+
+input[type=submit]:hover { 
+background-color: white; 
+position:center; 
+} 
+
+.container { 
+border-radius: 5px; 
+background-color:transparent; 
+padding: 20px; 
+} 
+</style> 
+</head> 
+<body> 
+<body style="background-color:AntiqueWhite;"> 
+<body> 
+<div class="container"> 
+<form action="transaction_history.php">
+<label for="Transfer to"><h2 style ="color:#002966;text-align:left;font-family: Times New Roman;"><b>Transfer To:</b></h2></label> 
+<select id="Choose" name="Choose"> 
+<option value="Choose">Choose</option> 
+<option value="Shubhra(40000)">Shubhra(40000)</option> 
+<option value="Kavita(50000)">Kavita(50000)</option> 
+<option value="Saumitra(30000)">Saumitra(30000)</option> 
+<option value="Pranjal(40000)">Pranjal(40000)</option> 
+<option value="Saurabh(50000)">Saurabh(50000)</option> 
+<option value="Rohan(50000)">Rohan(50000)</option> 
+<option value="David(30000)">David(30000)</option> 
+<option value="Nayna(40000)">Nayna(40000)</option> 
+<option value="Danilla(40000)">Danilla(40000)</option> 
+<option value="Avni(30000)">Avni(30000)</option> 
+</select> 
+<label for="Amount"><h2 style ="color:Black;text-align:left;font-family: Times New Roman;"><b>Amount</b></h2></label> 
+<input type="text" id="Amt" name="Amount" placeholder="Amount"> 
+<br> 
+<br> 
+<div class="container1"> 
+<h4><b><a href="transaction_history.php"><input type="Submit" value="Transfer" style ="color:#000033;text-align:center;font-family: Times New Roman;"</b></h4> 
+</form> 
+</div> 
+</style> 
+</body> 
+</head> 
+</html> 
