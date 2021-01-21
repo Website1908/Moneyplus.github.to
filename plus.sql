0d4cbf1f49e4a713556711a970e77c0c058bdf53
Website1908 <77770776+Website1908@users.noreply.github.com>
2021-01-21 13:03:00 +0530

    Add files via upload

---
+++ plus.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 21, 2021 at 05:46 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `plus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `sno` int(3) NOT NULL,
+  `sender` text NOT NULL,
+  `receiver` text NOT NULL,
+  `balance` int(8) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `balance` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'Shubhra', 'kulkarni@gmail.com', 40000),
+(2, 'Kavita', 'advani@gmail.com', 50000),
+(3, 'Saumitra', 'shah@gmail.com', 30000),
+(4, 'Pranjal', 'kapoor@gmail.com', 40000),
+(5, 'Saurabh', 'chopra@gmail.com', 50000),
+(6, 'Rohan', 'singh@gmail.com', 50000),
+(7, 'David', 'padukone@gmail.com', 30000),
+(8, 'Nayna', 'chawla@gmail.com', 40000),
+(9, 'Danilla', 'jonas@gmail.com', 40000),
+(10, 'Avni', 'pannu@gmail.com', 30000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
