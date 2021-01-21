a2761dcceb987692561042a9f82e603bb758eddb
Website1908 <77770776+Website1908@users.noreply.github.com>
2021-01-21 13:01:53 +0530

    Add files via upload

---
+++ Navbar.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+<title>MONEY PLUS</title> 
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+<meta content="text/html; charset=iso-8859-2" http-equiv="Content-Type"> 
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> 
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+
+<style> 
+
+.mySlides {display:none;} 
+
+body {font-family:Times New Roman;} 
+
+.navbar { 
+
+width:100%; 
+
+background-color: #f2f2f2; 
+
+overflow: auto; 
+
+} 
+.navbar a{ float:right; 
+padding:16px; 
+color:#000066; 
+text-decoration:none; 
+font-size:20px; 
+} 
+.navbar a:hover { 
+backgound-color:#0000cc; 
+} 
+@media screen and (max-width: 500px) 
+.navbar a { 
+float:none; 
+enter display:block; 
+} 
+} 
+</style> 
+</head> 
+<style> 
+body { 
+background-image:url('Bg.jpg'); 
+background-repeat:no-repeat; 
+background-attachment:fixed; 
+background-size:100% 100%; 
+} 
+</style> 
+<body> 
+<h4 style="color:#660066; text-align:left; font-family:Times New Roman;">Money Plus<i class="fa fa-university"></i></h4> 
+<div class="navbar"> 
+<a href ="transaction_history.php">Transaction History</a> 
+<a href ="transfer-money.php">Transfer Money</a> 
+<a href ="create-user.php">Create user</a> 
+<a href= "home.php">Home</a>  
+</div> 
